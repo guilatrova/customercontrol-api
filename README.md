c1c5b03c4f8fdf373cf581abfe51cac484990945
Guilherme Latrova <guilhermelatrova@outlook.com>
2017-01-12 00:29:40 -0200

    Added badge and admin to production env

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # customercontrol-api
 Django Rest Framework API + VSCode
+
+![Travis CI](https://travis-ci.org/guilatrova/customercontrol-api.svg?branch=master)
--- prototype/settings/production.py
+++ prototype/settings/production.py
@@ -9,3 +9,5 @@ ALLOWED_HOSTS = ['*']
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ADMINS = (('Guilherme Latrova','guilhermelatrova@hotmail.com'),)
