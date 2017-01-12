9e4b53c5b882cf4d528e149b7b05542ddbf2bfae
Guilherme Latrova <guilhermelatrova@outlook.com>
2017-01-12 00:45:56 -0200

    heroku test config

--- prototype/settings/local.py
+++ prototype/settings/local.py
@@ -3,3 +3,16 @@ from prototype.settings.base import *
 SECRET_KEY = '5a2619d6-dfbd-4b94-8fd4-cffa35cac9c3'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}'''
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
--- prototype/settings/production.py
+++ prototype/settings/production.py
@@ -1,8 +1,11 @@
 import os
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
 from prototype.settings.base import *
 
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
+DATABASES['default'] = dj_database_url.config()
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
@@ -10,4 +13,4 @@ ALLOWED_HOSTS = ['*']
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ADMINS = (('Guilherme Latrova','guilhermelatrova@hotmail.com'),)
+#ADMINS = (('Guilherme Latrova','guilhermelatrova@hotmail.com'),)
